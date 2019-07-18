2b4bb2819ea7033bf7a2ad99279d1691182530f6
Igor Rinkovec <igor.rinkovec@gmail.com>
2019-07-18 13:42:11 +0200

    new login

--- resources/views/student/login.blade.php
+++ resources/views/student/login.blade.php
@@ -5,8 +5,6 @@
 @section('bodyStyle') background-color: #26A65B; color: #fff; @endsection
 
 @section('content')
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/hr_HR/sdk.js#xfbml=1&version=v3.3&appId=1637256469877794&autoLogAppEvents=1"></script>
 <div class="container" style="padding-top: 150px">
     <div class="row">
         <div class="col-md-6 col-md-offset-3 text-center">
@@ -15,7 +13,9 @@
             </p>
             <p style="font-size: 20px;">Prijava je jednostavna, samo pritisni gumb ispod kako bi se prijavio/la putem Facebooka te prati upute.</p>
             <br><br>
-            <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
+            <a href="{{ route('student.login.go') }}">
+                <img style="height: 40px; width: auto;" src="{{ asset('img/fb-login.png') }}">
+            </a>
         </div>
     </div>
 </div>
