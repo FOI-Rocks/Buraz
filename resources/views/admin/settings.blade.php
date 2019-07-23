099cf3dd15baec68504895b814f6f4332ff98512
Matija Vuk <matija.vuk.97@gmail.com>
2019-07-23 09:42:12 +0200

    visual changes on settings page

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -4,26 +4,25 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-4 mt-2">
+        <div class="col-md-12 mt-2">
             <button type="button" onclick="executeActions();" class="btn btn-dark">Ugasi matching i resetiraj podatke
             </button>
         </div>
-        <div class="col-md-4 mt-2">
+        <div class="col-md-12 mt-2">
             <button type="button" onclick="startMatching();" class="btn btn-dark">Upali matching
             </button>
         </div>
-        <div class="col-md-4 mt-2">
-            <button type="button" onclick="notifyMatch();" class="btn btn-dark">Obavijesti korisnike
+        <div class="col-md-12 mt-2">
+            <button type="button" onclick="notifyMatch();" class="btn btn-dark">Obavijesti korisnike o dodijeljenjim burazima
             </button>
         </div>
-        <div class="col-md-4 mt-2">
-            <button type="button" onclick="clearConsole();" class="btn btn-dark">Obriši konzolu
-            </button>
-        </div>
+    </div>
     <p id="console-log">
 
     </p>
-
+    <div class="col-md-12 mt-2 text-right">
+        <button type="button" onclick="clearConsole();" class="btn btn-dark">Obriši konzolu
+        </button>
     </div>
     <script type="text/javascript" src="{{ URL::asset('js/settings.js') }}"></script>
 @endsection
